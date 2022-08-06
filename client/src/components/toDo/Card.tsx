58fc2f31d7a075dfc5ba2c0e15ce2c33f3cbb462
matty255 <89088205+matty255@users.noreply.github.com>
2022-08-06 16:17:35 +0900

    feat: updateToDo

--- client/src/components/toDo/Card.tsx
+++ client/src/components/toDo/Card.tsx
@@ -1,17 +1,21 @@
 import React from "react";
-import { ToDoProps, ToDoList } from "../../types/toDos";
+import { ToDoProps, ToDoList, NewToDo } from "../../types/toDos";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { deleteToDo } from "../../api/httpRequest";
+
 import instance from "../../api/instance";
 import axios from "axios";
 
 const Card = (data: ToDoProps) => {
   const [modify, setModify] = React.useState(false);
+  const [values, setValues] = React.useState<NewToDo>({
+    title: "",
+    content: "",
+  });
   const queryClient = useQueryClient();
   const token: string = localStorage.getItem("token") || "";
   const deleteMutation = useMutation(
     (id: string) =>
-      axios.delete(`http://localhost:8080/todos/${id}`, {
+      instance.delete(`/todos/${id}`, {
         headers: { Authorization: token },
       }),
     {
@@ -22,18 +26,64 @@ const Card = (data: ToDoProps) => {
   );
   const deleteHandler = (id: string) => {
     alert("삭제완료");
-
     deleteMutation.mutate(id);
   };
 
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.persist();
+    setValues((values) => ({
+      ...values,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  const updateMutation = useMutation(
+    (values: NewToDo) => instance.put(`/todos/${data.id}`, values),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(["todos"]);
+      },
+    }
+  );
+  const handleUpdate = () => {
+    setModify((state) => !state);
+    if (values.title === "" || values.content === "") {
+      values.title = data.title;
+      values.content = data.content;
+    }
+    updateMutation.mutateAsync(values).then((res) => console.log(res));
+  };
   return (
     <>
       <div className="flex">
         <div className="flex-col flex gap-3 mb-5">
-          {modify ? <input /> : <p className="text-xl"> {data.title}</p>}
-          {modify ? <input /> : <p className="text-xl"> {data.content} </p>}
+          {modify ? (
+            <input
+              name="title"
+              value={values.title || data.title}
+              onChange={handleChange}
+              required
+            />
+          ) : (
+            <p className="text-xl"> {data.title}</p>
+          )}
+          {modify ? (
+            <input
+              name="content"
+              value={values.content || data.content}
+              onChange={handleChange}
+              required
+            />
+          ) : (
+            <p className="text-xl"> {data.content} </p>
+          )}
         </div>
-        <button onClick={() => setModify((state) => !state)}>수정</button>
+
+        {modify ? (
+          <button onClick={handleUpdate}>수정완료</button>
+        ) : (
+          <button onClick={() => setModify((state) => !state)}>수정</button>
+        )}
         <button onClick={() => deleteHandler(data.id)}>삭제</button>
       </div>
     </>
